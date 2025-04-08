688c6d6bc0ce5d52b66441f6b6bd3425eba1e6e2
Ian Choi <ianw.choi@aol.com>
2025-04-08 23:09:57 +0900

    🎉 최초 업로드: 랜덤 성경 프로젝트

---
+++ script.js
@@ -0,0 +1,27 @@
+let bibleData = {};
+
+fetch("full_bible.json")
+  .then(response => response.json())
+  .then(data => {
+    bibleData = data;
+  });
+
+function showRandomChapter() {
+  const books = Object.keys(bibleData);
+  const book = books[Math.floor(Math.random() * books.length)];
+
+  const chapters = Object.keys(bibleData[book]);
+  const chapter = chapters[Math.floor(Math.random() * chapters.length)];
+
+  const verses = bibleData[book][chapter];
+  const outputDiv = document.getElementById("output");
+
+  let html = `<h2>${book} ${chapter}장</h2>`;
+  html += "<ul>";
+  for (const verseNum of Object.keys(verses)) {
+    html += `<li><strong>${verseNum}</strong>절: ${verses[verseNum]}</li>`;
+  }
+  html += "</ul>";
+
+  outputDiv.innerHTML = html;
+}
