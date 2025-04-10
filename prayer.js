45998e3511a8d08a50d511a88c34cde80000ddce
IanWChoi <ianwschoi@gmail.com>
2025-04-10 10:50:31 +0900

    성경읽으러가기 버튼 생성, 기도방 배경색 은은한 랜덤으로 추가

--- prayer.js
+++ prayer.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("pray-submit");
-    button.addEventListener("click", () => {
-      alert("방금 적은 기도는 서버에 전송되지 않으나, 하나님께 전송되었습니다.");
-    });
+  const pastelColors = [
+    "#fdfcf5", 
+    "#f9fbe7",
+    "#f0f9ff", 
+    "#fceff9", 
+    "#fffaf0", 
+    "#f3fdf3", 
+    "#fefefe", 
+    "#f3f8ff", 
+    "#fff5f7"  
+  ];
+
+  const randomColor = pastelColors[Math.floor(Math.random() * pastelColors.length)];
+  document.body.style.backgroundColor = randomColor;
+
+  const button = document.getElementById("pray-submit");
+  button.addEventListener("click", () => {
+    alert("방금 적은 기도는 서버에 전송되지 않으나, 하나님께 전송되었습니다.");
   });
-  
+});
